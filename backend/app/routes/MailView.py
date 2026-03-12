a3678c44e0b04a27cb3c3033cd501ed050f56f3e
Divyansh Singh Raikwar <theraikwar7@gmail.com>
2026-03-12 18:37:15 +0530

    remnoved the {email} keyword from the backend rout e but still not correcting or senduin the email correclty as still issue in route and have to confifyur etghe route correcty in backend routing path

--- backend/app/routes/MailView.py
+++ backend/app/routes/MailView.py
@@ -7,14 +7,14 @@ from app.models.gmailData import Email
 
 router = APIRouter()
 
-@router.get('/mails/sent/{email}')
+@router.get('/mails/sent/')
 def get_sent_mails(mail:str,db:Session=Depends(get_db)):
     email=(
         db.query(Email).filter(Email.sender==email)
         .order_by(Email.internal_date.desc()).all()
     )
     return email
-@router.get('/mails/recieved/{email}')
+@router.get('/mails/recieved/')
 def get_recieved_mails(mail:str,db:Session=Depends(get_db)):
     email=(db.query(Email)
            .filter(Email.bcc_recipients==email
