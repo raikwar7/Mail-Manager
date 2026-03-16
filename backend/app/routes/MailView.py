ee5490763cd0fdd2e46355bda0e6452314da77e7
Divyansh Singh Raikwar <theraikwar7@gmail.com>
2026-03-16 16:19:28 +0530

    the reciever details are not fethced correclty in cc,bcc or to recip have to configure another logic to save the relevant data or redefine the lofic to save the other data

--- backend/app/routes/MailView.py
+++ backend/app/routes/MailView.py
@@ -1,6 +1,7 @@
 
 from fastapi import FastAPI,requests,responses,APIRouter,Depends
 import os
+from sqlalchemy import or_
 from sqlalchemy.orm import Session
 from app.routes.user import get_db
 from app.models.gmailData import Email
@@ -8,16 +9,20 @@ from app.models.gmailData import Email
 router = APIRouter()
 
 @router.get('/mails/sent/')
-def get_sent_mails(mail:str,db:Session=Depends(get_db)):
+def get_sent_mails(email:str,db:Session=Depends(get_db)):
     email=(
         db.query(Email).filter(Email.sender==email)
         .order_by(Email.internal_date.desc()).all()
     )
     return email
 @router.get('/mails/recieved/')
-def get_recieved_mails(mail:str,db:Session=Depends(get_db)):
-    email=(db.query(Email)
-           .filter(Email.bcc_recipients==email
-                                   or Email.cc_recipients==email).order_by
-    (Email.internal_date.desc()).all())
-    return email
+def get_recieved_mails(email:str,db:Session=Depends(get_db)):
+    return(db.query(Email)
+           .filter(or_
+                   (Email.bcc_recipients.like(f"%{email}%"),
+                    Email.to_recipients.like(f"%{email}%"),
+                    Email.cc_recipients.like(f"%{email}%")
+                     )
+           ).order_by(Email.internal_date.desc())
+           .all()
+    )
