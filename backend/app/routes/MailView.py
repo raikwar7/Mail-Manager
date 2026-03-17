3fe95c3e69bcdabffdf456f4d818dbe6e2e92d98
Divyansh Singh Raikwar <theraikwar7@gmail.com>
2026-03-17 18:30:50 +0530

    acdded the backend route for the sent mail in one file route

--- backend/app/routes/MailView.py
+++ backend/app/routes/MailView.py
@@ -8,11 +8,11 @@ from app.models.gmailData import Email
 
 router = APIRouter()
 
-@router.get('/mails/sent/')
+@router.get('/mails/sent/{email}')
 def get_sent_mails(email:str,db:Session=Depends(get_db)):
     return (db.query(Email).filter(Email.sender.like(f"%{email}%"))
             .order_by(Email.internal_date.desc()).all())
-@router.get('/mails/recieved/')
+@router.get('/mails/recieved/{email}')
 def get_recieved_mails(email:str,db:Session=Depends(get_db)):
     return(db.query(Email)
            .filter(or_
